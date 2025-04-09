cea41e9bebca581b4f7f96d5629f5f20e77ed739
jbazanaguirre <jbazanaguirre@gmail.com>
2025-04-09 12:41:00 -0300

    Broadcasting trip request to all drivers

--- server/trips/consumers.py
+++ server/trips/consumers.py
@@ -43,6 +43,13 @@ class TaxiConsumer(AsyncJsonWebsocketConsumer):
         trip = await self._create_trip(data)
         trip_data = await self._get_trip_data(trip)
 
+        # Send rider requests to all drivers.
+        await self.channel_layer.group_send(group='drivers', message={
+            'type': 'echo.message',
+            'data': trip_data
+        })
+
+
         await self.send_json({
           'type': 'echo.message',
           'data': trip_data,
