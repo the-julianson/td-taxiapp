387dd43f6fa449e1ce7c72260fe8887e157ba4b8
jbazanaguirre <jbazanaguirre@gmail.com>
2025-04-10 00:15:50 -0300

    Refactor Cypress to use the server, have all the tests pass

--- client/cypress/e2e/authentication.cy.ts
+++ client/cypress/e2e/authentication.cy.ts
@@ -1,48 +1,29 @@
 /// <reference types="cypress" />
+const faker = require('faker');
+
+const randomEmail = faker.internet.email();
 
 const logIn = () => {
-  const { username, password } = Cypress.env('credentials');
+  const { password } = Cypress.env('credentials');
 
   // Capture HTTP requests.
-  cy.intercept('POST', 'log_in', {
-    statusCode: 200,
-    body: {
-      access: 'ACCESS_TOKEN',
-      refresh: 'REFRESH_TOKEN',
-    },
-  }).as('logIn');
+  cy.intercept('POST', 'log_in').as('logIn');
 
   // Log into the app.
   cy.visit('/#/log-in');
-  cy.get('input#username').type(username);
+  cy.get('input#username').type(randomEmail);
   cy.get('input#password').type(password, { log: false });
   cy.get('button').contains('Log in').click();
   cy.wait('@logIn');
 };
 
 describe('Authentication', function () {
-  it('Can log in.', function () {
-    logIn();
-    cy.hash().should('eq', '#/');
-    cy.get('button').contains('Log out');
-  });
-
   it('Can sign up.', function () {
     // new
-    cy.intercept('POST', 'sign_up', {
-      statusCode: 201,
-      body: {
-        id: 1,
-        username: 'gary.cole@example.com',
-        first_name: 'Gary',
-        last_name: 'Cole',
-        group: 'driver',
-        photo: '/media/images/photo.jpg',
-      },
-    }).as('signUp');
+    cy.intercept('POST', 'sign_up').as('signUp');
 
     cy.visit('/#/sign-up');
-    cy.get('input#username').type('gary.cole@example.com');
+    cy.get('input#username').type(randomEmail);
     cy.get('input#firstName').type('Gary');
     cy.get('input#lastName').type('Cole');
     cy.get('input#password').type('pAssw0rd', { log: false });
@@ -54,6 +35,12 @@ describe('Authentication', function () {
     cy.get('button').contains('Sign up').click();
     cy.wait('@signUp'); // new
     cy.hash().should('eq', '#/log-in');
+  });
+
+  it('Can log in.', function () {
+    logIn();
+    cy.hash().should('eq', '#/');
+    cy.get('button').contains('Log out');
   });
 
   it('Cannot visit the login page when logged in.', function () {
@@ -78,7 +65,7 @@ describe('Authentication', function () {
   });
 
   it('Shows an alert on login error.', function () {
-    const { username, password } = Cypress.env('credentials');
+    const { password } = Cypress.env('credentials');
     cy.intercept('POST', 'log_in', {
       statusCode: 400,
       body: {
@@ -89,7 +76,7 @@ describe('Authentication', function () {
       },
     }).as('logIn');
     cy.visit('/#/log-in');
-    cy.get('input#username').type(username);
+    cy.get('input#username').type(randomEmail);
     cy.get('input#password').type(password, { log: false });
     cy.get('button').contains('Log in').click();
     cy.wait('@logIn');
@@ -118,7 +105,7 @@ describe('Authentication', function () {
       },
     }).as('signUp');
     cy.visit('/#/sign-up');
-    cy.get('input#username').type('gary.cole@example.com');
+    cy.get('input#username').type(randomEmail);
     cy.get('input#firstName').type('Gary');
     cy.get('input#lastName').type('Cole');
     cy.get('input#password').type('pAssw0rd', { log: false });
--- server/taxi/settings.py
+++ server/taxi/settings.py
@@ -41,11 +41,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.postgres',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'trips',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -166,3 +168,7 @@ SIMPLE_JWT = {
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = Path(BASE_DIR / 'media')
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3001",
+]
